d00f9752e592e9d007d04aa58df5293f23d101ae
Daniel <borstel.daniel@googlemail.com>
2015-05-07 01:26:32 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -57,9 +57,9 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "basicBot",
         language: "english",
-        startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
-        startupEmoji: false, // true or false
+        startupCap: 50, // 1-200
+        startupVolume: 50, // 0-100
+        startupEmoji: true, // true or false
         cmdDeletion: true,
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         maximumAfk: 120,
@@ -72,8 +72,8 @@
         maximumLocktime: 10,
         cycleGuard: true,
         maximumCycletime: 10,
-        voteSkip: false,
-        voteSkipLimit: 10,
+        voteSkip: true,
+        voteSkipLimit: 3,
         timeGuard: true,
         maximumSongLength: 10,
         autodisable: true,
